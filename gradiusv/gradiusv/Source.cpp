86c972874b954887544e0d9ad0d0ce2be1d6f76e
r_kawano <r_kawano@asojuku.ac.jp>
2019-05-26 20:56:54 +0900

    つまり、こう

--- gradiusv/gradiusv/Source.cpp
+++ gradiusv/gradiusv/Source.cpp
@@ -14,6 +14,17 @@ struct BendLazerPos {
 	BendLazerPos(const Position2f& p,const Vector2f& v) :pos(p),vel(v) {}
 };
 
+struct ControlPoint {
+	Position2f pos = Position2f(100, 100);
+	Vector2f vel = Vector2f(0, 0);
+	 int life = 600;
+	ControlPoint() {}
+	ControlPoint(const Position2f& p) :pos(p) {}
+	ControlPoint(const Position2f& p, const Vector2f& v) :pos(p), vel(v) {}
+	void Update() { life--; }
+	bool isDead() { life >= 0; }
+};
+
 vector<BendLazerPos> CalculateCubicBezier(const Position2f& pos1,
 	const Position2f& pos2, 
 	const Position2f& pos3, 
@@ -35,6 +46,13 @@ vector<BendLazerPos> CalculateCubicBezier(const Position2f& pos1,
 	return ret;
 }
 
+void LoopEndProcess() {
+	DrawFormatString(10, 20, 0xffffff, "FPS=%f", GetFPS());
+	DrawFormatString(10, 40, 0xffffff, "DrawCall=%d", GetDrawCallCount());
+
+	ScreenFlip();
+}
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	ChangeWindowMode(true);
@@ -43,22 +61,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
 
+	auto vicviperH=LoadGraph("img/vicviper.png");
+
+	array<int,20> screenH;
+	for (auto& s : screenH) {
+		s = MakeScreen(640, 480);
+	}
+	
 	Position2 pos(320, 240);
 
-	array<Position2f, 4> ctrlPositions;
+	list<ControlPoint> ctrlPnts(4);
 
-	for (int i = 0; i < ctrlPositions.size(); ++i) {
-		ctrlPositions[i].x = pos.x + i * 50;
-		ctrlPositions[i].y = pos.y;// -i * 80 + (i*i) * 30;
+	int cnt = 0.;
+	for (auto& p : ctrlPnts) {
+		p.pos.x = pos.x + cnt * 50;
+		p.pos.y = pos.y;// -i * 80 + (i*i) * 30;
+		++cnt;
 	}
 
 	array<float, 3> lengthes;
-	for (int i = 0; i < lengthes.size(); ++i) {
-		lengthes[i] = (ctrlPositions[i + 1] - ctrlPositions[i]).Length();
+	auto ctrlpnt0 = ctrlPnts.begin();
+	auto ctrlpnt1 = ctrlpnt0;
+	++ctrlpnt1;
+	for (auto& l : lengthes) {
+		l = (ctrlpnt1->pos - ctrlpnt0->pos).Length();
+		++ctrlpnt0;
+		++ctrlpnt1;
 	}
 	array<Position2f, 3> targetpositions;
+	auto ctrlpnt = ctrlPnts.begin();
+	++ctrlpnt;
 	for (int i = 0; i < targetpositions.size(); ++i) {
-		targetpositions[i] =ctrlPositions[i+1];
+		targetpositions[i] = ctrlpnt->pos;
+		++ctrlpnt;
 	}
 
 
@@ -70,15 +105,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	auto lastangle = angle;
 	char keystate[256];
 
-
-	vector<BendLazerPos> bs=CalculateCubicBezier(ctrlPositions[0],
-		ctrlPositions[1],
-		ctrlPositions[2],
-		ctrlPositions[3],
-		20);
-	list<BendLazerPos> lazer;
-	copy(bs.begin(), bs.end(), back_inserter(lazer));
-
+	ctrlpnt = ctrlPnts.begin();
+	vector<BendLazerPos> bs=CalculateCubicBezier(ctrlpnt->pos,
+		(++ctrlpnt)->pos,
+		(++ctrlpnt)->pos,
+		(++ctrlpnt)->pos,
+		10);
+	
+//	copy(bs.begin(), bs.end(), back_inserter(lazer));
+	int frame = 0;
+	int drawframe = 0;
 	while (ProcessMessage()==0) {
 		ClearDrawScreen();
 
@@ -98,19 +134,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		if (keystate[KEY_INPUT_Z]) {
-			angle += 0.12f;
+			angle += 1.0f/6.f;
+			ctrlPnts.emplace_front( pos.ToFloatVec());
+			frame = 0;
 		}
 		if (keystate[KEY_INPUT_X]) {
-			angle -= 0.12f;
+			angle -= 1.0f/6.f;
+			ctrlPnts.emplace_front(pos.ToFloatVec());
+			frame = 0;
 		}
 
-		ctrlPositions[0] = pos.ToFloatVec();
+		++frame;
+
 
 		//ターゲット座標設定
 		auto length = 0.0f;
 		for (int i = 0; i < targetpositions.size(); ++i) {
 			length += lengthes[i];
-			targetpositions[i] = ctrlPositions[0] + Vector2f(cos(angle)*length, sin(angle)*length);
+			targetpositions[i] = pos.ToFloatVec() + Vector2f(cos(angle)*length, sin(angle)*length);
 		}
 
 		//線形補間
@@ -121,66 +162,212 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//		ctrlPositions[i + 1] += (targetpositions[i] - ctrlPositions[i + 1]).Normalized()*velocities[i];
 		//	}
 		//}
+		if (ctrlPnts.empty()) {
+			LoopEndProcess();
+			continue;
+		}
+		ctrlpnt0 = ctrlPnts.begin();
+		ctrlpnt1 = ctrlpnt0;
+		++ctrlpnt1;
+
+
 
 		//球面線形補間
-		for (int i = 0; i < targetpositions.size(); ++i) {
+		//for (int i = 0; i < targetpositions.size(); ++i) 
+		{
 /*			if ((ctrlPositions[i + 1] - targetpositions[i]).Length() < velocities[i]*1.2f) {
 				ctrlPositions[i + 1] = targetpositions[i];
 			}else*/{
 				//線形補間座標
-				auto tmp = (ctrlPositions[i + 1] -ctrlPositions[i]).Normalized();
-				auto tmp2 = (targetpositions[i]-ctrlPositions[i]).Normalized();
-				auto cross = Cross(tmp, tmp2);
-				auto angle = sin(cross);
-				angle = copysign(min(omegas[i], abs(angle)), angle);
-				ctrlPositions[i + 1] = ctrlPositions[i] + (tmp*Rotate(angle))*lengthes[i];
-				//auto tmp=ctrlPositions[i + 1] + (targetpositions[i] - ctrlPositions[i + 1]).Normalized()*velocities[i];				
-				//ctrlPositions[i + 1] = ctrlPositions[i] + (tmp - ctrlPositions[i]).Normalized()*lengthes[i];
+
+				//auto tmp = (ctrlpnt0->pos -pos.ToFloatVec()).Normalized();
+				//auto tmp2 = (targetpositions[0]-pos.ToFloatVec()).Normalized();
+				//auto cross = Cross(tmp, tmp2);
+				//auto angle = sin(cross);
+				//angle = copysign(min(omegas[0], abs(angle)), angle);
+				//ctrlpnt0->pos = pos.ToFloatVec() + (tmp*Rotate(angle)) *lengthes[0];
+				//tmp= ctrlpnt1->pos + (targetpositions[i] - ctrlpnt1->pos).Normalized()*velocities[i];
+				//ctrlpnt1->pos = ctrlpnt0->pos + (tmp - ctrlpnt0->pos).Normalized()*lengthes[i];
 			}
+			//++ctrlpnt0;
+			//++ctrlpnt1;
 		}
-
-		//コントロールポイント描画
-		//for (const auto& p : ctrlPositions) {
-		//	DrawCircle(p.x, p.y, 3, 0xffffff, true);
-		//}
-		//for (int i = 1; i < ctrlPositions.size();++i) {
-		//	const auto& p = ctrlPositions[i];
-		//	const auto& prep = ctrlPositions[i-1];
-		//	DrawLine(prep.x, prep.y, p.x, p.y, 0xffffff,2);
-		//}
-
-		//ベジェ曲線計算
-		
-		if (lastangle != angle) {
-			auto bz = CalculateCubicBezier(ctrlPositions[0],
-				ctrlPositions[1],
-				ctrlPositions[2],
-				ctrlPositions[3],
-				10);
-
-			auto rit = bz.rbegin();
-			for (; rit != bz.rend(); ++rit) {
-				lazer.push_front(*rit);
-			}
-			
+		if (ctrlPnts.empty()) {
+			LoopEndProcess();
+			continue;
 		}
-		for (auto& b : lazer) {
-			if ((b.pos - pos.ToFloatVec() ).Length()==0.0f) {
-				b.vel = Vector2f(cos(angle), sin(angle));
+		for (auto& cp : ctrlPnts) {
+			if ((cp.pos - pos.ToFloatVec()).Length() == 0.0f) {
+				cp.vel = Vector2f(cos(angle), sin(angle))*4;
 			}
 			else {
-				b.vel = (b.pos - pos.ToFloatVec()).Normalized();
+				cp.vel = (cp.pos - pos.ToFloatVec()).Normalized()*4;
 			}
 		}
 
-		for (auto& b : lazer) {
-			b.pos += b.vel;
+		for (auto& cp : ctrlPnts) {
+			cp.pos += cp.vel;
+			cp.Update();
 		}
 
-		//ベジェ描画
-		for (const auto& p : lazer) {
-			DrawCircle(p.pos.x, p.pos.y, 1, 0x88aaff, true);
+
+		//for (auto& b : lazer) {
+		//	if ((b.pos - pos.ToFloatVec() ).Length()==0.0f) {
+		//		b.vel = Vector2f(cos(angle), sin(angle));
+		//	}
+		//	else {
+		//		b.vel = (b.pos - pos.ToFloatVec()).Normalized();
+		//	}
+		//}
+
+		//for (auto& b : lazer) {
+		//	b.pos += b.vel;
+		//}
+
+
+		////コントロールポイント描画
+		//for (const auto& p : ctrlPnts) {
+		//	DrawCircle(p.pos.x, p.pos.y, 3, 0xffffff, true);
+		//}
+
+		//ctrlpnt0 = ctrlPnts.begin();
+		//ctrlpnt1 = ctrlpnt0;
+		//++ctrlpnt1;
+		//DrawLine(pos.x, pos.y,
+		//	ctrlpnt0->pos.x, ctrlpnt0->pos.y, 0xffffff, 2);
+		//for (; ctrlpnt1 != ctrlPnts.end(); ++ctrlpnt0, ++ctrlpnt1) {
+		//	if (ctrlpnt0->pos.x >= -120 && ctrlpnt0->pos.x <= 760 &&
+		//		ctrlpnt0->pos.y >= -120 && ctrlpnt0->pos.y <= 600 &&
+		//		ctrlpnt1->pos.x >= -120 && ctrlpnt1->pos.x <= 760 &&
+		//		ctrlpnt1->pos.y >= -120 && ctrlpnt1->pos.y <= 600) {
+		//		DrawLine(ctrlpnt0->pos.x, ctrlpnt0->pos.y,
+		//			ctrlpnt1->pos.x, ctrlpnt1->pos.y, 0xffffff, 2);
+		//	}
+
+		//}
+
+		//不必要なコントロールポイントを削除
+		ctrlPnts.erase( std::remove_if(ctrlPnts.begin(), ctrlPnts.end(), [](const ControlPoint& pred) {
+			int x = pred.pos.x;
+			int y = pred.pos.y;
+			return x < -200 || x>840 || y < -200 || y>680 || pred.life<0;
+			}),ctrlPnts.end()) ;
+
+		list<BendLazerPos> lazer;
+		//ベジェ曲線計算(今回はコントロールポイントすべて通したいため)
+		//中間点は自前で計算する
+		//std::list<ControlPoint>::reverse_iterator lastit = ctrlPnts.rbegin();
+		if (ctrlPnts.size() <= 2) {
+			LoopEndProcess();
+			continue;
 		}
+		auto cpitend = ctrlPnts.rend();
+		--cpitend;
+		--cpitend;
+		--cpitend;
+		for (auto cpit = ctrlPnts.rbegin(); cpit != cpitend; ++cpit) {
+
+
+
+			auto p0 = cpit == ctrlPnts.rbegin() ? pos.ToFloatVec() : cpit->pos;
+			auto cpit1 = cpit;
+			auto p1 = cpit == ctrlPnts.rbegin() ? cpit->pos : (++cpit1)->pos;
+			auto cpit2 = cpit1;
+			auto p2 = (++cpit2)->pos;
+			auto cpit3 = cpit2;
+			auto p3 = (++cpit3)->pos;
+
+			if (p0.x < -120 || p0.x > 760 ||
+				p0.y < -120 || p0.y > 600 ||
+				p1.x < -120 || p1.x > 760 ||
+				p1.y < -120 || p1.y > 600 ||
+				p2.x < -120 || p2.x > 760 ||
+				p2.y < -120 || p2.y > 600 ||
+				p3.x < -120 || p3.x > 760 ||
+				p3.y < -120 || p3.y > 600 
+				) {
+				continue;
+			}
+
+			//中間点といっても中点では、長い辺と短い辺の差別化はできないため
+			//割合を計算しておく
+			//まず長さを計算
+			auto len0 = (p1 - p0).Length();
+			auto len1 = (p2 - p1).Length();
+			auto len2 = (p3 - p2).Length();
+
+			//割合を計算(m1はm0もしくはm2が分かれば自動的にわかるため必要ない)
+			auto m0 = len0 + len1 == 0.0f ? 0.0f : len0 / (len0 + len1);
+			auto m2 = len1 + len2 == 0.0f ? 0.0f : len1 / (len1 + len2);
+			
+			//中点計算
+			auto mpos0 = (p0 + p1)*0.5f;
+			auto mpos1 = (p1 + p2)*0.5f;
+			auto mpos2 = (p2 + p3)*0.5f;
+
+			//中間点計算
+			auto mp0 = mpos0+(mpos1 - mpos0)*m0;
+			auto mp1 = mpos1+(mpos2 - mpos1)*m2;
+
+			mp0 = mpos1 - mp0 + p1;
+			mp1 = mpos1 - mp1 + p2;
+
+
+			//auto cp3 = cpit;
+			//++cp3;
+			//if (cp3 == ctrlPnts.rend())break;
+
+			std::vector<BendLazerPos> bz;
+			//if (lastit == cp0) {//一番最初
+				bz = CalculateCubicBezier(p1,
+					mp0,
+					mp1,
+					p2,
+					10);
+			//}
+			//else {
+				//bz = CalculateCubicBezier(lastit->pos,
+				//	cp0->pos,
+				//	cp1->pos,
+				//	(cp2 == ctrlPnts.rend()) ? pos.ToFloatVec() : cp2->pos,
+				//	10);
+			//}
+			
+			//lastit = cp2;
+			std::copy(bz.begin(), bz.end(), back_inserter(lazer));
+		}
+			//auto rit = bz.rbegin();
+			//for (; rit != bz.rend(); ++rit) {
+			//	lazer.push_front(*rit);
+			//}
+		//	
+		//}
+		//for (auto& b : lazer) {
+		//	if ((b.pos - pos.ToFloatVec() ).Length()==0.0f) {
+		//		b.vel = Vector2f(cos(angle), sin(angle));
+		//	}
+		//	else {
+		//		b.vel = (b.pos - pos.ToFloatVec()).Normalized();
+		//	}
+		//}
+
+		//for (auto& b : lazer) {
+		//	b.pos += b.vel;
+		//}
+
+		//ベジェ描画
+		//for (const auto& p : lazer) {
+		//	if ( p.pos.x > 0 && p.pos.x <= 640 && p.pos.y > 0 && p.pos.y <= 480) {
+		//		DrawCircle(p.pos.x, p.pos.y, 10, 0x88aaff, true);
+		//	}
+		//}
+		if (lazer.empty()) {
+			LoopEndProcess();
+			continue;
+		}
+
+		SetDrawScreen(screenH[drawframe]);
+		ClearDrawScreen();
 		auto it = lazer.begin();
 		it++;
 		for (; it!=lazer.end();++it) {
@@ -188,15 +375,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			auto it2 = it;
 			--it2;
 			const auto& prep = *it2;
-			DrawLine(prep.pos.x, prep.pos.y, p.pos.x, p.pos.y, 0x88aaff,2);
+			if (prep.pos.x > 0 && prep.pos.x <= 640 && p.pos.x > 0 && p.pos.x <= 640 &&
+				prep.pos.y > 0 && prep.pos.y <= 480 && p.pos.y > 0 && p.pos.y <= 480) {
+				DrawLine(prep.pos.x, prep.pos.y, p.pos.x, p.pos.y, 0x88aaff, 3);
+			}
+		}
+		////
+		DrawLine(pos.x, pos.y, lazer.back().pos.x, lazer.back().pos.y, 0x88aaff, 3);
+		SetDrawScreen(DX_SCREEN_BACK);
+		DrawGraph(0, 0, screenH[drawframe], true);
+		for (int i = 0; i < screenH.size(); ++i) {
+			int idx = (drawframe + i) % screenH.size();
+			SetDrawBlendMode(DX_BLENDMODE_ADD,26*(i+1));
+			GraphFilter(screenH[idx], DX_GRAPH_FILTER_GAUSS, 8, 100);
+			DrawGraph(0, 0, screenH[idx], true);
+
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND,0);
 		}
 		
-		DrawCircle(pos.x, pos.y, 5, 0xff0000, false,3);
-
+		DrawRotaGraph(pos.x, pos.y, 2.0f, 0.0f, vicviperH, true);
 
 		lastangle = angle;
 
-		ScreenFlip();
+		drawframe = (drawframe + 1) % screenH.size();
+
+
+		LoopEndProcess();
 	}
 
 	return 0;
