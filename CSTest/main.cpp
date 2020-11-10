033856a09f2ad695d78a7c55255f55472590c4fc
Ryuichi Kawano <r_kawano@asojuku.ac.jp>
2020-11-10 19:32:12 +0900

    CSテスト

--- CSTest/main.cpp
+++ CSTest/main.cpp
@@ -28,8 +28,34 @@ namespace {
 	ID3D12GraphicsCommandList * cmdList_;
 
 }
+std::vector<float> uavdata(4 * 4 * 4);
+HRESULT CreateUAVBuffer(ID3D12Device* dev,ID3D12Resource*& res) {
+	HRESULT result= S_OK;
+	D3D12_HEAP_PROPERTIES heapProp = {};
+	heapProp.Type = D3D12_HEAP_TYPE_DEFAULT;
+	D3D12_RESOURCE_DESC resDesc = {};
+	resDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+	resDesc.Format = DXGI_FORMAT_UNKNOWN;
+	resDesc.Width = sizeof(uavdata[0]) * uavdata.size();
+	resDesc.DepthOrArraySize = 1;
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Height = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	result = dev->CreateCommittedResource(&heapProp, D3D12_HEAP_FLAG_NONE, &resDesc, D3D12_RESOURCE_STATE_UNORDERED_ACCESS, nullptr,
+		IID_PPV_ARGS(&res));
+	assert(SUCCEEDED(result));
+	return result;
+}
+
 int main() {
 	HRESULT result = S_OK;
+
+	ID3D12Debug* debug;
+	D3D12GetDebugInterface(IID_PPV_ARGS(&debug));
+	debug->EnableDebugLayer();
+	debug->Release();
 	result = D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&dev_));
 	assert(SUCCEEDED(result));
 
@@ -40,6 +66,8 @@ int main() {
 		OutputFromErrorBlob(errBlob);
 	}
 
+	ID3D12Resource* uavBuffer = nullptr;
+	CreateUAVBuffer(dev_,uavBuffer);
 	cmdList_->Dispatch(1, 1, 1);
 
 	assert(SUCCEEDED(result));
