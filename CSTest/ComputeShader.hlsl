9533275c42eebd15376c16144e7a1960f6659577
Ryuichi Kawano <r_kawano@asojuku.ac.jp>
2020-11-20 16:14:53 +0900

    コンピュートシェーダはじめの一歩

--- CSTest/ComputeShader.hlsl
+++ CSTest/ComputeShader.hlsl
@@ -1,4 +1,11 @@
-[numthreads(1, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+struct Buffer_t {
+	float val;
+};
+
+RWStructuredBuffer< Buffer_t> OutBuff:register(u0);
+
+[numthreads(4, 4, 4)]
+void main( uint3 id : SV_DispatchThreadID )
 {
+	OutBuff[id.x * 4 * 4 + id.y * 4 + id.z].val = id.x * 4 * 4 + id.y * 4 + id.z;
 }
--- CSTest/main.cpp
+++ CSTest/main.cpp
@@ -4,6 +4,7 @@
 #include<cassert>
 #include<vector>
 #include<string>
+#include<iostream>
 
 using namespace std;
 
@@ -30,7 +31,8 @@ namespace {
 	ID3D12Resource* res_;//計算データ用リソース
 	ID3D12Device* dev_;//デバイスオブジェクト
 	ID3D12GraphicsCommandList * cmdList_;//コマンドリスト
-	ID3D12PipelineState* _pipeline;
+	ID3D12CommandQueue* cmdQue_;
+	ID3D12PipelineState* pipeline_;
 
 }
 std::vector<float> uavdata(4 * 4 * 4);//テストUAVデータ
@@ -61,6 +63,29 @@ HRESULT CreateUAVBuffer(ID3D12Device* dev,ID3D12Resource*& res) {
 	return result;
 }
 
+HRESULT CreateCopyBuffer(ID3D12Device* dev, ID3D12Resource*& res) {
+	HRESULT result = S_OK;
+	D3D12_HEAP_PROPERTIES heapProp = {};
+	heapProp.Type = D3D12_HEAP_TYPE_READBACK;
+	D3D12_RESOURCE_DESC resDesc = {};
+	resDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+	resDesc.Format = DXGI_FORMAT_UNKNOWN;
+	resDesc.Width = sizeof(uavdata[0]) * uavdata.size();
+	resDesc.DepthOrArraySize = 1;
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Height = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	result = dev->CreateCommittedResource(&heapProp, 
+		D3D12_HEAP_FLAG_NONE, 
+		&resDesc, 
+		D3D12_RESOURCE_STATE_COPY_DEST, nullptr,
+		IID_PPV_ARGS(&res));
+	assert(SUCCEEDED(result));
+	return result;
+}
+
 int main() {
 	HRESULT result = S_OK;
 
@@ -78,9 +103,23 @@ int main() {
 		OutputFromErrorBlob(errBlob);
 	}
 
+	D3D12_DESCRIPTOR_RANGE range[1] = {};
+	range[0].BaseShaderRegister = 0;
+	range[0].NumDescriptors = 1;
+	range[0].OffsetInDescriptorsFromTableStart = 0;
+	range[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_UAV;
+	range[0].RegisterSpace = 0;
+
+	D3D12_ROOT_PARAMETER rp[1] = {};
+	rp[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+	rp[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+	rp[0].DescriptorTable.NumDescriptorRanges = 1;
+	rp[0].DescriptorTable.pDescriptorRanges = range;
+
+	D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
 	D3D12_ROOT_SIGNATURE_DESC rootSigDesc = {};
-	rootSigDesc.NumParameters = 0;
-	rootSigDesc.pParameters = nullptr;
+	rootSigDesc.NumParameters = 1;
+	rootSigDesc.pParameters = rp;
 	rootSigDesc.NumStaticSamplers = 0;
 	rootSigDesc.pStaticSamplers = nullptr;
 	rootSigDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_STREAM_OUTPUT;
@@ -89,27 +128,93 @@ int main() {
 	pldesc.CS.pShaderBytecode = csBlob->GetBufferPointer();
 	pldesc.CS.BytecodeLength = csBlob->GetBufferSize();
 	pldesc.NodeMask = 0;
+	ID3DBlob* rootSigBlob = nullptr;
+	D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1_0, &rootSigBlob, &errBlob);
+	dev_->CreateRootSignature(0, rootSigBlob->GetBufferPointer(), rootSigBlob->GetBufferSize(), IID_PPV_ARGS(&pldesc.pRootSignature));
 
-	pldesc.pRootSignature;
-	dev_->CreateComputePipelineState(&pldesc, IID_PPV_ARGS(&_pipeline));
+	result = dev_->CreateComputePipelineState(&pldesc, IID_PPV_ARGS(&pipeline_));
+
+	ID3D12DescriptorHeap* descHeap = nullptr;
+	D3D12_DESCRIPTOR_HEAP_DESC descHeapDesc = {};
+	descHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	descHeapDesc.NodeMask = 0;
+	descHeapDesc.NumDescriptors = 1;
+	descHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	result = dev_->CreateDescriptorHeap(&descHeapDesc, IID_PPV_ARGS(&descHeap));
 
 
-	ID3D12CommandAllocator* cmdAlloc = nullptr;
-	//コマンドアロケータ作成
-	dev_->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_COMPUTE, IID_PPV_ARGS(&cmdAlloc));
-	//コマンドリスト作成
-	dev_->CreateCommandList(0,D3D12_COMMAND_LIST_TYPE_COMPUTE, cmdAlloc,nullptr,IID_PPV_ARGS(&cmdList_));
-
-	
-
-
-
-	ID3D12Resource* uavBuffer = nullptr;
-	CreateUAVBuffer(dev_,uavBuffer);
-	cmdList_->Dispatch(1, 1, 1);
 
 	assert(SUCCEEDED(result));
+	D3D12_COMMAND_QUEUE_DESC queDesc = {};
+	queDesc.NodeMask = 0;
+	queDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	queDesc.Priority = 0;
+	queDesc.Type = D3D12_COMMAND_LIST_TYPE_COMPUTE;
+	dev_->CreateCommandQueue(&queDesc, IID_PPV_ARGS(&cmdQue_));
+	ID3D12CommandAllocator* cmdAlloc_ = nullptr;
+	//コマンドアロケータ作成
+	result = dev_->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_COMPUTE, IID_PPV_ARGS(&cmdAlloc_));
+	assert(SUCCEEDED(result));
+	//コマンドリスト作成
+	result = dev_->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_COMPUTE, cmdAlloc_, pipeline_, IID_PPV_ARGS(&cmdList_));
+	//cmdList_->Close();
+	//cmdAlloc_->Reset();
 
+
+	assert(SUCCEEDED(result));
+	ID3D12Resource* uavBuffer = nullptr;
+	CreateUAVBuffer(dev_, uavBuffer);
+
+	D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+	uavDesc.Format = DXGI_FORMAT_UNKNOWN;
+	uavDesc.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
+	uavDesc.Buffer.NumElements = 4 * 4 * 4;
+	uavDesc.Buffer.StructureByteStride = sizeof(float);
+	uavDesc.Buffer.FirstElement = 0;
+	uavDesc.Buffer.Flags = D3D12_BUFFER_UAV_FLAG_NONE;
+	dev_->CreateUnorderedAccessView(uavBuffer, nullptr, &uavDesc, descHeap->GetCPUDescriptorHandleForHeapStart());
+
+	cmdList_->SetComputeRootSignature(pldesc.pRootSignature);
+	ID3D12DescriptorHeap* descHeaps[] = { descHeap };
+	cmdList_->SetDescriptorHeaps(1, descHeaps);
+	cmdList_->SetComputeRootDescriptorTable(0, descHeap->GetGPUDescriptorHandleForHeapStart());
+	cmdList_->Dispatch(1, 1, 1);
+
+	ID3D12Resource* cpyBuffer = nullptr;
+	CreateCopyBuffer(dev_, cpyBuffer);
+
+	D3D12_RESOURCE_BARRIER barrier = {};
+	barrier.Transition.pResource = uavBuffer;
+	barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+	barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_COPY_SOURCE;
+	barrier.Transition.Subresource = 0;
+	barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+	cmdList_->ResourceBarrier(1, &barrier);
+	cmdList_->CopyResource(cpyBuffer, uavBuffer);
+
+	cmdList_->Close();
+	ID3D12Fence* fence_ = nullptr;
+	dev_->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence_));
+	UINT64 fenceValue_ = 0;
+	ID3D12CommandList* cmdLists[] = { cmdList_ };
+	cmdQue_->ExecuteCommandLists(1, cmdLists);
+	cmdQue_->Signal(fence_, ++fenceValue_);
+
+
+	while (fence_->GetCompletedValue() < fenceValue_) {
+		;
+	}
+
+	float* mappedRes = nullptr;
+	D3D12_RANGE rng = {};
+	rng.Begin = 0;
+	rng.End = uavdata.size() * sizeof(float);
+	cpyBuffer->Map(0, &rng, (void**)(&mappedRes));
+	copy_n(mappedRes,  uavdata.size(), uavdata.data());
+	cpyBuffer->Unmap(0, nullptr);
 	dev_->Release();
+	for (auto& d : uavdata) {
+		cout << d << endl;
+	}
 	return 0;
 }
