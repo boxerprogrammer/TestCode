7e84062caa1b81d33f8c132a3cea833b2eca4220
Ryuichi Kawano <zan56011@nifty.com>
2021-10-13 02:24:11 +0900

    微調整

--- ReadLaigter/ReadLaigter/main.cpp
+++ ReadLaigter/ReadLaigter/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void KDrawGraph(int texH,int normH,int shaderH,int x,int y,int width,int height) {
+void MyDrawGraph(int x,int y,int width,int height) {
 	array<VERTEX2DSHADER, 4> verts;
 	for (auto& v : verts) {
 		v.rhw = 1.0f;
@@ -29,7 +29,22 @@ void KDrawGraph(int texH,int normH,int shaderH,int x,int y,int width,int height)
 	verts[3].u = 1.0f;
 	verts[3].v = 1.0f;
 	DrawPrimitive2DToShader(verts.data(), verts.size(), DX_PRIMTYPE_TRIANGLESTRIP);
+}
 
+void MyDrawGraph(int texH, int normH, int shaderH, int x, int y, int width, int height) {
+	SetUseTextureToShader(0, texH);
+	SetUseTextureToShader(1, normH);
+	SetUsePixelShader(shaderH);
+	MyDrawGraph(x, y, width, height);
+}
+
+void MyDrawGraph(int x, int y, int graphH,int normH,int shaderH) {
+	SetUseTextureToShader(0, graphH);
+	SetUseTextureToShader(1, normH);
+	SetUsePixelShader(shaderH);
+	int w, h;
+	GetGraphSize(graphH, &w, &h);
+	MyDrawGraph(graphH,-1,shaderH,x, y, w, h);
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE,  LPSTR, int) {
@@ -43,13 +58,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,  LPSTR, int) {
 	auto ashuraNormalH = LoadGraph(L"img/hotoke_asyura_n.png");
 	auto pshandle = LoadPixelShader(L"testps.pso");
 	auto cbuffer=CreateShaderConstantBuffer(sizeof(float)*4);
+	float* gangle = static_cast<float*>(GetBufferShaderConstantBuffer(cbuffer));
 	float angle = 0.0f;
 	while (ProcessMessage() != -1) {
 		ClearDrawScreen();
 		angle += 0.01f;
 		SetUseTextureToShader(0, ashuraH);
 		SetUseTextureToShader(1, ashuraNormalH);
-		float* gangle=static_cast<float*>(GetBufferShaderConstantBuffer(cbuffer));
+		
 		gangle[0] = angle;
 		UpdateShaderConstantBuffer(cbuffer);
 		SetShaderConstantBuffer(cbuffer, DX_SHADERTYPE_PIXEL, 0);
@@ -57,7 +73,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE,  LPSTR, int) {
 		SetUsePixelShader(pshandle);
 		int w, h;
 		GetGraphSize(ashuraH,&w,&h);
-		KDrawGraph(ashuraH, ashuraNormalH, pshandle, 150, 50, w, h);
+		MyDrawGraph(ashuraH, ashuraNormalH, pshandle, 150, 50, w, h);
 		ScreenFlip();
 	}
 	return 0;
