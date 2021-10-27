ab7e9844d9fd23bd8463aa365998a1014398e0a0
kawano <tekito@s.asojuku.ac.jp>
2021-10-27 10:54:53 +0900

    いろいろ修正

--- SpriteOutline/SpriteOutline/spriteoutline_ps.hlsl
+++ SpriteOutline/SpriteOutline/spriteoutline_ps.hlsl
@@ -19,22 +19,35 @@ SamplerState smp       : register( s0 ) ;
 Texture2D    tex       : register( t0 ) ;
 Texture2D    norm       : register( t1 ) ;
 
-#define DIV_SQRT_2 0.70710678118
+
+float OutlineCheck(float2 uv){
+		#define DIV_SQRT_2 0.70710678118
 float2 directions[8] = {float2(1, 0), float2(0, 1), float2(-1, 0), float2(0, -1),
   float2(DIV_SQRT_2, DIV_SQRT_2), float2(-DIV_SQRT_2, DIV_SQRT_2),
   float2(-DIV_SQRT_2, -DIV_SQRT_2), float2(DIV_SQRT_2, -DIV_SQRT_2)};
+	float maxAlpha = 0.0f;
+	for(uint level=1;level<=8;++level){
+		// for(uint index = 0; index<8; index++){
+		// 	float2 sampleUV = uv + directions[index] * float(level)*0.01;
+		// 	maxAlpha = max(maxAlpha, tex.Sample(smp,sampleUV).a/float(level));
+		// }
+		float2 sampleUV = uv + float2(-DIV_SQRT_2, -DIV_SQRT_2)* float(level)*0.008;
+		maxAlpha = max(maxAlpha, tex.Sample(smp,sampleUV).a/float(level));
+	}
+	return maxAlpha;
+}
+
 // main関数
 PS_OUTPUT main( PS_INPUT PSInput )
 {
+
 	PS_OUTPUT PSOutput ;
 	
 	float4 col=tex.Sample(smp,PSInput.uv);
-	float maxAlpha = 0;
-	for(uint index = 0; index<8; index++){
-		float2 sampleUV = PSInput.uv + directions[index] * 0.01;
-		maxAlpha = max(maxAlpha, tex.Sample(smp,sampleUV).a);
-	}
+	float maxAlpha=OutlineCheck(PSInput.uv);
+
 	PSOutput.Output.rgb = lerp(float3(0.5, 0.5, 1), col.rgb, col.a);
 	PSOutput.Output.a=max(col.a,maxAlpha);
+	
 	return PSOutput ;
 }
