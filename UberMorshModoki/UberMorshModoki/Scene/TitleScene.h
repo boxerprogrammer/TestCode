25a533bf4f708bd488029f4140e6fa7602e2783e
boxerprogrammer <g@email.com>
2022-12-12 13:44:15 +0900

    uber

--- UberMorshModoki/UberMorshModoki/Scene/TitleScene.h
+++ UberMorshModoki/UberMorshModoki/Scene/TitleScene.h
@@ -7,17 +7,18 @@ class TitleScene : public Scene {
 private:
 	using UpdateFunc_t=void (TitleScene::* )();
 	using DrawFunc_t = void (TitleScene::*)();
-	void BeginUpdate();
-	void BeginDraw();
-	void MainUpdate();
-	void MainDraw();
-	void EndUpdate();
-	void EndDraw();
+	void BeginUpdate() {};
+	void BeginDraw() {};
+	void MainUpdate() {};
+	void MainDraw() {};
+	void EndUpdate() {};
+	void EndDraw() {};
 	UpdateFunc_t updateFunc_ = &TitleScene::BeginUpdate;
 	UpdateFunc_t drawFunc_ = &TitleScene::BeginDraw;
 	int fadeTime_ = 0;
 public:
-	TitleScene(SceneManager& manager);
+	TitleScene(SceneManager& manager) :Scene(manager) {}
+	~TitleScene() {}
 	virtual void Update(const InputState& input)override;
 	virtual void Draw()override;
 };
